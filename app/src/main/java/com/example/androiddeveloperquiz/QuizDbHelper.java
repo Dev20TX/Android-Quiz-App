b8afe1eb32216def966be4dd322841842f4d0d04
dev20tx <dev20tx@gmail.com>
2019-12-17 17:16:21 -0600

    Project commit

---
+++ app/src/main/java/com/example/androiddeveloperquiz/QuizDbHelper.java
@@ -0,0 +1,94 @@
+package com.example.androiddeveloperquiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.androiddeveloperquiz.QuizContract.*;
+
+
+import java.util.ArrayList;
+
+
+import androidx.annotation.Nullable;
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "AndroidQuiz.db";
+    private static final int DATABASE_VERSION = 3;
+
+    private SQLiteDatabase db;
+
+    public QuizDbHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question("Which is called at the final point of the Activity Lifecycle?", "A    onCreate", "B    onDestroy", "C    onResume", 2);
+        addQuestion(q1);
+        Question q2 = new Question("Select a core component of Android", "A    Boolean", "B    Activity", "C    Swift", 2);
+        addQuestion(q2);
+        Question q3 = new Question("Does a service provide a user interface?", "A    No", "B    Yes", "C    Yes, but let's go surfing.", 1);
+        addQuestion(q3);
+        Question q4 = new Question("What does ABD stand for?", "A    Android Device Bridge", "B    Android Debug Break", "C    Android Debug Bridge", 3);
+        addQuestion(q4);
+        Question q5 = new Question("Which will save data during a screen rotate?", "A    onSaveIntegerStore", "B    onSaveInstanceState", "C    onSaveIndexState", 2);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+
+    public ArrayList<Question> getAllQuestions() {
+        ArrayList<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionList;
+    }
+}
